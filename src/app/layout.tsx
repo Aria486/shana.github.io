b39c70670a54d30ac19319b8d33838f7529499e8
Aria486 <842915974@qq.com>
2024-08-02 13:49:49 +0800

    修改 eslint 报错

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { Theme } from "@/components";
 import "@/assets/styles/globals.css";
 import "@/assets/styles/globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "shana blog",

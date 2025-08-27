6592c66d4436909d54288c2dd5b93ed04270a8d9
Aria486 <842915974@qq.com>
2025-08-27 12:26:31 +0800

    SonarQube pullrequest テスト

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -36,3 +36,7 @@ export const linearInterpolation = (
 ): number => {
   return p0 + t * (p1 - p0);
 };
+
+export const clamp = (value: number, min: number, max: number): number => {
+  return Math.min(Math.max(value, min), max);
+};
